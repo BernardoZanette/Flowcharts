1a54c9389d4f7a20c9613e0ca95184e470714b1b
BernardoZanette <bernardo7206@gmail.com>
2024-03-05 14:39:15 -0300

    Tentativas para solucao do problema com steps pais e filho

--- front-flowcharts/src/app/core/pages/flowcharts-page/flowcharts-page.component.ts
+++ front-flowcharts/src/app/core/pages/flowcharts-page/flowcharts-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FlowchartResponse, FlowchartService } from '../../../services/flowchart.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-flowcharts-page',
@@ -15,7 +16,9 @@ import { FormsModule } from '@angular/forms';
 })
 export class FlowchartsPageComponent {
 
-  constructor(private flowchartService: FlowchartService) {}
+  constructor(private flowchartService: FlowchartService, private renderer: Renderer2) {}
+
+  // @ViewChild('flowchart', { static: true }) flowchartDiv: ElementRef;
 
   pais!: [];
   flowchartStructure!: FlowchartResponse[];
@@ -27,6 +30,7 @@ export class FlowchartsPageComponent {
     this.getFlowcharts()
     this.flowchartId = 1
     this.getFlowchartStructure() 
+    // console.log(this.flowchartDiv.nativeElement);
   }
 
   // TO-DO: trocar data-type depois de subscribe
@@ -47,12 +51,36 @@ export class FlowchartsPageComponent {
     let structure = this.flowchartStructure
     structure.forEach((step) => { 
       const stepParentValues = Object.values(step.stepParentIds)
-      console.log(stepParentValues)
+      console.log(step)
+
+      // IS SON
       if (stepParentValues.length !== 0) {
         this.actualParentId = stepParentValues[0]['step_parent_id']
+        console.log('filho: ' + step.id + ' com o pai: ' + this.actualParentId)
+        
+        // const conjunto_pai = document.getElementById(this.actualParentId.toString()).parentNode
+
+        // const div_filho = document.getElementById(step.id.toString())
+
+        // this.renderer.appendChild(conjunto_pai, div_filhos)
+        // this.renderer.appendChild(div_filhos, div_filho)
+        
       }
+      // IS FATHER:
       else {
-        console.log('pai: '+ step.id)
+        console.log('pai: ' + step.id)
+        
+        // const pai = document.getElementById(step.id.toString());
+        
+        // const conjunto = this.renderer.createElement('div');
+        // conjunto.setAttribute('class', 'conjuntoPaiFilho')
+
+        // this.renderer.appendChild((pai.parentNode), conjunto)
+        // this.renderer.appendChild(conjunto, pai)
+
+        // var div_filhos = this.renderer.createElement('div');
+        // div_filhos.setAttribute('class', 'filhos')
+
       }
     })
   }
